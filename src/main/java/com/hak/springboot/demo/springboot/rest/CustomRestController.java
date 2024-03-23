0c6bf4c772f2e5ecb64b8124557f2a59cf223778
HamzaAkbar067 <hakbar067@gmail.com>
2024-03-24 00:04:58 +0100

    import Spring Boot Actuators

--- src/main/java/com/hak/springboot/demo/springboot/rest/CustomRestController.java
+++ src/main/java/com/hak/springboot/demo/springboot/rest/CustomRestController.java
@@ -1,13 +1,18 @@
 package com.hak.springboot.demo.springboot.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CustomRestController {
+    @Value("${coach.name}")
+    private String coachName;
+
     @GetMapping("/")
+
     public String sayHello() {
-        return "Hello World";
+        return this.coachName;
     }
 
 
